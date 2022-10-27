7460446798157f79745681e93e7ec3e9d07aeecb
Avishek <82322452+avishek-hub@users.noreply.github.com>
2022-10-27 23:10:10 +0530

    Add files via upload

---
+++ Type Conversion and Coercion/conversion&coercion.js
@@ -0,0 +1,16 @@
+console.log (Boolean(0));
+console.log (Boolean(''));
+console.log (Boolean(undefined));
+console.log (Boolean(null));
+console.log (Boolean(NaN));
+console.log (Boolean('Avishek'));
+
+const money = 0;
+
+if (money > 0)
+{
+    console.log(`You have money`);
+}
+else {
+console.log(`You don't have sufficient money`);
+}
